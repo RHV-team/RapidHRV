5974bec8bb52dfcee3b4ac4212bdc6c191aebc80
peterakirk <p.kirk@ucl.ac.uk>
2022-01-09 17:16:13 +0000

    Updated unit-test

--- tests/test_rapidhrv.py
+++ tests/test_rapidhrv.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import rapidhrv as rhv
 
 
@@ -8,6 +7,12 @@ def test_pipeline():
     signal = rhv.get_example_data()
     preprocessed = rhv.preprocess(signal)
     result = rhv.analyze(preprocessed)
+    bpm = np.nanmean(result['BPM'])
+    rmssd = np.nanmean(result['RMSSD'])
+
     assert result is not None
     assert len(result) > 0
     assert not np.all(np.isnan(result["BPM"]))
+    assert (59 < bpm < 61)
+    assert (26 < rmssd < 28)
+
