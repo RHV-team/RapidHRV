765244d6cf3d16261542eb24868764176d8b8679
Alexander Davidson Bryan <alxdb@pm.me>
2022-01-13 23:49:31 +0000

    Run code formatter

--- tests/test_rapidhrv.py
+++ tests/test_rapidhrv.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 import rapidhrv as rhv
 
 
@@ -7,12 +8,11 @@ def test_pipeline():
     signal = rhv.get_example_data()
     preprocessed = rhv.preprocess(signal)
     result = rhv.analyze(preprocessed)
-    bpm = np.nanmean(result['BPM'])
-    rmssd = np.nanmean(result['RMSSD'])
+    bpm = np.nanmean(result["BPM"])
+    rmssd = np.nanmean(result["RMSSD"])
 
     assert result is not None
     assert len(result) > 0
     assert not np.all(np.isnan(result["BPM"]))
-    assert (59 < bpm < 61)
-    assert (26 < rmssd < 28)
-
+    assert 59 < bpm < 61
+    assert 26 < rmssd < 28
